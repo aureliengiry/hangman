b2e8db4025e2cb9faa661038aa81f0842e7205ec
HeahDude <heah@heahprod.com>
2017-06-16 17:26:20 +0200

    Added a ComposerDataCollector

--- src/AppBundle/DataCollector/ComposerDataCollector.php
+++ src/AppBundle/DataCollector/ComposerDataCollector.php
@@ -13,7 +13,27 @@ class ComposerDataCollector extends DataCollector
      */
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-        // $lock = ???
+        $lock = json_decode(file_get_contents(__DIR__.'/../../../composer.lock'), true);
+
+        $this->data = [
+            'packages_count' => count($lock['packages']),
+            'packages_dev_count' => count($lock['packages-dev']),
+            'packages' => array_merge($lock['packages'], $lock['packages-dev'])
+        ];
+    }
+
+    public function getPackagesCount()
+    {
+        return $this->data['packages_count'];
+    }
+
+    public function getPackagesDevCount()
+    {
+        return $this->data['packages_dev_count'];
+    }
+    public function getPackages()
+    {
+        return $this->data['packages'];
     }
 
     /**
